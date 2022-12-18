22a4916e71fa9cdc902d7d3d4a2026c88d4b2b6d
Sevval Kurtgoz <sevval1834@outlook.com>
2022-12-18 20:58:59 +0300

    router örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sablon from './sayfalar/Sablon'
+import Anasayfa from './sayfalar/Anasayfa'
+import Hakkimizda from './sayfalar/Hakkimizda'
+import Iletisim from './sayfalar/Iletisim'
+import Sayfayok from './sayfalar/Sayfayok'
 
 function App() {
+
   return (
-    <div className="App">
-     Merhaba!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Sablon />}>
+          <Route index element={<Anasayfa />} />
+          <Route path="hakkimizda" element={<Hakkimizda />} />
+          <Route path="iletisim" element={<Iletisim />} />
+          <Route path="*" element={<Sayfayok />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
---
+++ src/sayfalar/Anasayfa.jsx
@@ -0,0 +1,11 @@
+function Anasayfa() {
+
+    return (
+        <section>
+            <h1>Ana sayfa</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias repellendus modi accusantium beatae amet! Nisi expedita enim laboriosam, ratione deserunt suscipit dolore officia in exercitationem minus magnam sunt explicabo.</p>
+        </section>
+    )
+}
+
+export default Anasayfa
