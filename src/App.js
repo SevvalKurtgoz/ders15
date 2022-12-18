78563a875b4519a14bd4f95172e31d5d19a7e7a1
Sevval Kurtgoz <sevval1834@outlook.com>
2022-12-18 16:58:39 +0300

    useref örneği 1 tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
 
 function App() {
+  const [yazi, yaziGuncelle] = React.useState("");
+  const sayac = React.useRef(0); // sayac.current = 0
+
+  function sayacGuncelle() {
+    sayac.current = sayac.current + 5;
+    console.log(sayac.current)
+  }
+
   return (
-    <div className="App">
-     Merhaba!
-    </div>
+    <>
+      <input
+        type="text"
+        value={yazi}
+        onChange={(e) => yaziGuncelle(e.target.value)}
+      />
+
+      <button onClick={ sayacGuncelle }>Güncelle</button>
+    </>
   );
 }
 
