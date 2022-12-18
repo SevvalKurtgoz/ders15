f4f3e1dde242d700e65171a3b5bc1f5fc12bd3e2
Sevval Kurtgoz <sevval1834@outlook.com>
2022-12-18 20:12:27 +0300

    usecallback yapıldı

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React from "react";
+import Todos from "./Todos";
 
 function App() {
+  const [sayac, sayacGuncelle] = React.useState(0);
+  const [yapilacaklar, yapilacaklarGuncelle] = React.useState([]);
+
+  const arttir = () => {
+    sayacGuncelle((oncekiDeger) => oncekiDeger + 1);
+  };
+
+  const isEkle = React.useCallback( () => {
+    yapilacaklarGuncelle((oncekiListe) => [...oncekiListe, "Yeni yapılacak iş"]);
+  }, []  )
+
   return (
-    <div className="App">
-     Merhaba!
-    </div>
+    <>
+      <Todos yapilacaklar={yapilacaklar} isEkle={isEkle} />
+
+      <hr />
+
+      <div>
+        Sayaç: {sayac}
+        <button onClick={arttir}>+</button>
+      </div>
+    </>
   );
 }
 
