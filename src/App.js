e52db652cf0803b414a19ca470618b895505a6d3
Sevval Kurtgoz <sevval1834@outlook.com>
2022-12-18 17:28:16 +0300

    useref 2 örneği

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from "react";
 
 function App() {
+  let eleman1 = React.useRef() //eleman1.current = 
+
+  function yaziGuncelle() {
+    eleman1.current.textContent = "İkinci yazı"
+  }
+
   return (
     <div className="App">
-     Merhaba!
+      <p ref={eleman1}>İlk yazı</p>
+
+      <button onClick={yaziGuncelle}>Güncelle</button>
     </div>
   );
 }
